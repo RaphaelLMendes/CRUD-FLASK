89bd19489800ce9c78a8fdc28e89a9104663b95d
Raphael Lucas Mendes <rapmendes@deloitte.com>
2021-08-01 21:07:38 -0300

    finished crud app

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,38 @@
 from flask import Flask, render_template
-
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from os import path
+
+#creating db object
+db = SQLAlchemy()
+DB_NAME = "tasks.db"
 
 def create_app():
-    app = Flask(__name__)
-    app.secret_key = "Hello World"
+    ''' Function responsible for creating the web app and define the env variables '''
 
+    # creating flask app
+    app = Flask(__name__)
+
+    # defining environment config | Development
+    app.config['SECRET_KEY'] = "hello world"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    # Initiating DB in app
+    db.init_app(app)
+
+    from .models import Task
+
+    #creates DB file
+    create_database(app)
+
+    #adding blueprint routes
     from .routes import routes
     app.register_blueprint(routes, url_prefix="/")
 
     return app
+
+def create_database(app):
+    ''' Function responsible for creating db file if doesnt exist '''
+
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
--- website/routes.py
+++ website/routes.py
@@ -1,8 +1,60 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Task
+from . import db
 
 routes = Blueprint('routes', __name__)
 
 
 @routes.route("/")
 def home():
-    return render_template("base_template.html")
+    return render_template("home.html")
+
+
+@routes.route("/list")
+def list():
+    list = Task.query.all()
+    return render_template("list.html", list=list)
+
+
+@routes.route("/new-task", methods=['GET', 'POST'])
+def new_task():
+    if request.method == 'POST':
+        task = request.form.get('task')
+
+        if len(task)==0:
+            return redirect(url_for('routes.new_task'))
+        else:
+            new_task_add = Task(task=task)
+
+            db.session.add(new_task_add)
+            db.session.commit()
+            return redirect(url_for('routes.list'))
+
+    else:
+        return render_template("new_task.html")
+
+
+@routes.route("/edit-task/<id>", methods=["GET", "POST"])
+def edit_task(id):
+    current_task = Task.query.filter_by(id=id).first()
+
+    if request.method == 'POST':
+        new_task = request.form.get('task')
+        if len(new_task) == 0:
+            return redirect(url_for('routes.edit_task', id=id))
+        else:
+            current_task.task = new_task
+            db.session.commit()
+            return redirect(url_for('routes.list'))
+
+    else:
+        return render_template("new_task.html", current_task=current_task)
+
+
+@routes.route("/delete-task/<id>", methods=["GET", "POST"])
+def delete_task(id):
+    current_task = Task.query.filter_by(id=id).first()
+    db.session.delete(current_task)
+    db.session.commit()
+    return redirect(url_for('routes.list'))
+
